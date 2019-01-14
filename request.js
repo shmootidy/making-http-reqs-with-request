2c4fbeed8910ab6ae33b7d2b7b440b139ab8abf6
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 21:54:40 +0000

    Got things working, for the most part. Gonna mess with the considerations now.

--- request.js
+++ request.js
@@ -3,9 +3,10 @@ var fs = require('fs');
 
 request.get('https://sytantris.github.io/http-examples/future.jpg')
   .on('error', function(err){
-    throw err;
+    console.log("There was an error. Specifically, error ", err);
+    return null;
   })
   .on('response', function(response){
-    console.log('Response Status Code: ', response.statusCode);
+    console.log('Response Status Message: ', response.statusMessage + '\n' + 'Content Type: ', response.headers['content-type']);
   })
-  .pipe(fs.createWriteStream('./downloaded.html'));
+  .pipe(fs.createWriteStream('./future.jpg'));
