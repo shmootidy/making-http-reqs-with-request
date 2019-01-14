5f2702d2081c8b6a8faa2948d2d8e8f713b9a0ec
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 21:59:50 +0000

    Okay, I think I've got it done. Going to confirm with a mentor. I tried to break things by passing a faulty URL, which gave me the appropriate response message but still ran the rest of the code, so that'll need to be fixed, but not now!

--- request.js
+++ request.js
@@ -3,10 +3,19 @@ var fs = require('fs');
 
 request.get('https://sytantris.github.io/http-examples/future.jpg')
   .on('error', function(err){
-    console.log("There was an error. Specifically, error ", err);
-    return null;
+    console.log('There was an error. Specifically, error ', err);
+    throw err;
   })
   .on('response', function(response){
     console.log('Response Status Message: ', response.statusMessage + '\n' + 'Content Type: ', response.headers['content-type']);
   })
-  .pipe(fs.createWriteStream('./future.jpg'));
+  .on('end', function(){
+    console.log('Downloading image...')
+  })
+  .pipe(fs.createWriteStream('./future.jpg'))
+  .on('finish', function(){
+    console.log('Download complete.');
+  })
+
+// considerations:
+// - I already know 'finish' is used for write and 'end' for read, so I added a 'finish' after .pipe.
